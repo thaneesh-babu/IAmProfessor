c964c1f1f4c6eb63c50ae00baf95bbe875271159
Thaneesh Babu <63804742+thaneesh-babu@users.noreply.github.com>
2021-09-06 18:03:04 -0400

    successfully fetches API

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
 let api_url = "https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow"
 
+function display(data, keyword) {
+    var length= Object.keys(data.items).length;
+    for (let question = 0; question < length; question++) {
+        if (JSON.stringify(data.items[question].title).includes(keyword)) {
+            document.write(JSON.stringify(data.items[question].title) + " - " + JSON.stringify(data.items[question].link));
+            document.write("<br>");
+        }
+    }
+}
+
 function getKeyword() {
+
     let inputKeyword = document.getElementById("searchbar").value
-    document.write(inputKeyword) //printing keyword just to see if the button function works
+
+    fetch(api_url) 
+    .then(response=>response.json())
+
+    .then(data=>display(data, inputKeyword))
+
 }
 
 //Figure out a way to fetch the API URL in JS. Refer to the API link to see how the JSON data looks like. 
